98dce7ae783510b9d5198412b7cf457f0f578111
Dennjes <dennis.schmueser@tms-hl.org>
2021-02-05 09:27:27 +0100

    Neues WELTDesign

---
+++ Greenfoot-Game/World2.java
@@ -0,0 +1,10 @@
+import greenfoot.*;
+
+public class World2 extends World {
+    public World2() {
+        super(20, 15, 50);
+        Greenfoot.setSpeed(50);
+        setBackground("images/Background.png");
+        
+    } 
+}

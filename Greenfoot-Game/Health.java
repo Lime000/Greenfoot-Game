d0a6caf2fb9c2765131258fcd34bf604f5c685fd
Dennjes <dennis.schmueser@tms-hl.org>
2021-02-12 09:26:09 +0100

    Health System

---
+++ Greenfoot-Game/Health.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Health here.
+ * Lebensanzeige für den Protagonisten
+ * @Dennjes aka Hackermann 
+ * @version 0.1
+ */
+public class Health extends Actor
+
+{
+    int heart;
+    /**
+     * Act - do whatever the Health wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if (heart == 0) {
+            
+        }
+    }    
+}

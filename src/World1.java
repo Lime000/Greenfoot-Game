61b25d78cfa177ba66f5917e5979d25f2bc1a005
Josh Bürgel <josh.buergel@tms-hl.org>
2021-03-16 10:39:36 +0100

    added images

--- src/World1.java
+++ src/World1.java
@@ -1,4 +1,5 @@
 import greenfoot.*;
+import java.util.Scanner;
 public class World1 extends Worlds {
     public void prepare() {
         super.prepare();
@@ -6,5 +7,10 @@ public class World1 extends Worlds {
         addObject(goal,17,12);
         Player player = new Player(this);
         addObject(player,2,2);
+    }
+    public void act(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.next()+ "d");
+        showText(scan.next(),5 , 5);
     }
 }
